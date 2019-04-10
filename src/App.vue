cc7759ff1fe1019905fcab4500f7d750f405a5ec
nickwest <njwest9@gmail.com>
2019-04-10 15:31:53 +0800

    :fire: :rocket: :fire:

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld :msg="message"/>
+    <input type="text" v-model="message" />
+    <button @click="alertMessage">Alert</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return {
+      message: 'Hello World!'
+    }
+  },
+  methods: {
+    alertMessage(){
+      alert(this.message)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
